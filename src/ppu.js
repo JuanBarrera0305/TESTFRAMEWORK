5b23d2c6be9fa7e86708240e8df5d2afc06e3cb3
JuanBarrera0305 <120711482+JuanBarrera0305@users.noreply.github.com>
2024-04-03 14:00:32 -0400

    first commit

---
+++ src/ppu.js
@@ -0,0 +1,22 @@
+class Bison {
+    add(a, b) {
+      return a + b;
+    }
+  
+    subtract(a, b) {
+      return a - b;
+    }
+  
+    multiply(a, b) {
+      return a * b;
+    }
+  
+    divide(a, b) {
+      if (b === 0) {
+        throw new Error("Can't divide by 0'");
+      }
+      return a / b;
+    }
+  }
+  
+  export default Bison;
